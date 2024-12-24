1c5b3dcfc41f1a9b21bc99539df8789748d776ea
Kundan Gahalot <gahalotkundan@gmail.com>
2024-12-24 10:55:56 +0530

    Initial commit

---
+++ src/Components/Cart/CartIcon.js
@@ -0,0 +1,13 @@
+const CartIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+    >
+      <path d="M3 3h2l3.6 7.59L5.25 17H19a1 1 0 0 0 0-2H6.42l1.1-2h9.48a1 1 0 0 0 .92-.61l3.24-7.24a1 1 0 0 0-.92-1.39H5.21L4.27 1H1v2h2zm6 15a1.5 1.5 0 1 0 1.5 1.5A1.5 1.5 0 0 0 9 18zm8 0a1.5 1.5 0 1 0 1.5 1.5A1.5 1.5 0 0 0 17 18z" />
+    </svg>
+  );
+};
+
+export default CartIcon;
---
+++ src/Components/Layout/HeaderCartButton.js
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../Store/cart-context";
+
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOffCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOffCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
